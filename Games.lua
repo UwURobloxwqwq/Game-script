64c082b952070e408d213e864c44f8961f27045e
UwURobloxwqwq <144137727+UwURobloxwqwq@users.noreply.github.com>
2025-01-06 01:34:48 +0700

    Update and rename Scripts.lua to Games.lua

--- Games.lua
+++ Games.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet(""))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UwURobloxwqwq/Game-script/main/Scripts-all.lua"))()
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
